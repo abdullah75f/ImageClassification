0a1ce4dfbd60bab5f95634d6c5799e874d05c183
Abdullah.F <abdullah75farid@gmail.com>
2025-03-18 16:18:55 +0300

    Initial commit

---
+++ image-classifier/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EmotionDetector from "./display";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <EmotionDetector />
+    </div>
+  );
+};
+
+export default App;
